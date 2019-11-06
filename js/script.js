56e38744b9ec87e761aaa25efdcdba9171960678
Vuckoslav <mijauv@gmail.com>
2019-11-06 20:56:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ class UI {
 
   addBookToList(book) {
     const list = document.getElementById('book-list');
-    //create tr element
+    
     const row = document.createElement('tr');
-    //insert cols
+    
     row.innerHTML = `
       <td>${book.title}</td>
       <td>${book.author}</td>
@@ -23,20 +23,20 @@ class UI {
   }
 
   showAlert(message, className) {
-    //create div
+    
     const div = document.createElement('div');
-    //add classes
+    
     div.className = `alert ${className}`;
-    //add text
+    
     div.appendChild(document.createTextNode(message));
-    //get parent
+    
     const container = document.querySelector('.col-md-8');
-    //get form
+    
     const form = document.querySelector('#book-form');
-    //insert alert
+    
     container.insertBefore(div, form);
 
-    // Timeout after 3 sec
+    
     setTimeout(function() {
       document.querySelector('.alert').remove();
     }, 1500);
@@ -47,7 +47,7 @@ class UI {
       target.parentElement.parentElement.remove();
 
       const ui = new UI();
-      //show message
+      
       ui.showAlert('Book Removed!', 'success');
     }
   }
@@ -61,49 +61,49 @@ class UI {
 
 
 
-//event listeners
+
 document.getElementById('book-form').addEventListener('submit', function(e) {
 
-  //get form values
+  
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const isbn = document.getElementById('isbn').value;
 
-  // instantiate book
+  
   const book = new Book(title, author, isbn);
 
-  // instantiate UI
+  
   const ui = new UI();
 
-  //validate input
+  
   if (title === '' || author === '' || isbn === '') {
-    //error alert
+    
     ui.showAlert('Please fill in all fields', 'error');
   } else {
 
-    //add book to list
+    
     ui.addBookToList(book);
 
-    //clear fields
+    
     ui.clearFields();
 
-    //show success
+    
     ui.showAlert('Book Added!', 'success');
   }
 
   e.preventDefault();
 });
 
-// event listeners for delete
+
 document.getElementById('book-list').addEventListener('click', function(e) {
-  // instantiate UI
+  
   const ui = new UI();
 
   ui.deleteBook(e.target);
 
   e.preventDefault();
 })
-  //filtering
+  
 
   $(document).ready(function(){
   $("#search").on("keyup", function() {
